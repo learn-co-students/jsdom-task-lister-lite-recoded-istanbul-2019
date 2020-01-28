32e49e1c9a68327811e8abcffdd8ae74882c6a62
mmelihdogan <doganmmelih@gmail.com>
2020-01-28 15:59:59 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    function createNewTask() {
+        fname = document.forms[0].fname.value;
+        alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
+    }
+
+
 });
