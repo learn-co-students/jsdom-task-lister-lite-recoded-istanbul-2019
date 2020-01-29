74cd3cac6374e22e0b363961b870c0b94d7364ec
Ammar <ammar.n.sammour@gmail.com>
2020-01-29 13:35:14 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    function todo() {
+        let newItem = document.createElement('li')
+        newItem.innerText = document.querySelector('input[type="text"]').value
+        document.querySelector("#tasks").appendChild(newItem);
+
+    }
+    document.querySelector('input[type="submit"]').addEventListener("click", function(e) {
+        e.preventDefault();
+        todo();
+    })
 });
