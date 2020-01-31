a7c152375f26433f0663b8e44a63ac3b03a5938c
merve gumuskaya <mervegumuskaya@gmail.com>
2020-01-31 19:53:07 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let submit = document.querySelector("#create-task-form input[type=submit]");
+let text = document.querySelector(`#create-task-form input[type=text]`);
+let tasks = document.querySelector(`#tasks`);
+let li = document.createElement(`li`);
+
+submit.addEventListener("click", function(event) {
+    event.preventDefault();
+    li.innerHTML = text.value;
+    tasks.appendChild(li);
+},false);
+
