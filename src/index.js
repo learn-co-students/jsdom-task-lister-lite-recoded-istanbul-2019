62b51bcff81f21ff05257b7b7824fdad5a9b1853
Hakan Yildiz <hakan@re-coded.com>
2020-02-05 14:55:29 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code 
+  
+  document.addEventListener("click",function(e){
+    e.preventDefault();
+    
+    const creatingList = document.createElement("li");
+    creatingList.innerText = document.getElementById("new-task-description").value;
+    
+    document.getElementById("tasks").appendChild(creatingList);
+ 
+    
+  });
+   
 });
