eef7cbae6934848c5a6472313d0c605156e184c6
abdurrahman rajab <rajab-2@windowslive.com>
2020-03-06 23:18:14 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const addBtn = document.querySelector('[type=submit]');
+  const list = document.querySelector('#tasks');
+  addBtn.addEventListener("click", function(event) {
+    const task = document.querySelector('[type=text]').value;
+    event.preventDefault();
+    let item = document.createElement('li');
+    item.innerHTML = task + ' <button data-description="' + task + '">delete</button>';
+    let button = item.getElementsByTagName('button')[0];
+    button.addEventListener("click", function(event) {
+      item.remove();
+    });
+    list.append(item);
+
+  });});
