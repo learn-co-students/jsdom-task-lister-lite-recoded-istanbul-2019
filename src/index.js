0a52f58d46c58fcca42620e7ad5f1da223be1ace
allan <allansaleh95@gmail.com>
2020-03-31 08:51:45 +0300

    Finished with adding the functionality of adding tasks to the list. I also added a functionality to remove any item you click on.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let ul = document.getElementById("tasks");
+  document.getElementById("submit-button").addEventListener("click",function(event){
+    event.preventDefault();
+    let li = document.createElement("li");
+    let textValue = document.getElementById("new-task-description").value;
+    li.appendChild(document.createTextNode(textValue));
+    li.setAttribute("class","ta");
+    ul.appendChild(li);
+  });
+
+  ul.addEventListener("click",function(e){
+    if(e.target && e.target.matches("li.ta")){
+      e.target.id = "del";
+      let d = document.getElementById("del");
+      ul.removeChild(d);
+    }
+  })
+
 });
