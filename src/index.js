1743a0514d319c97117bddd971bb04b9125539d4
Halit Batur <halitfuatbatur@gmail.com>
2020-04-09 14:28:20 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  function add(){
+    let newElement = document.createElement('li');
+    newElement.innerText = document.querySelector('#new-task-description').value;
+    document.querySelector('#tasks').appendChild(newElement);
+    document.querySelector('#new-task-description').value = "";
+  }
+
+  document.querySelector("#create-task-form").addEventListener("submit", function (e){
+  e.preventDefault();
+  add();
+ })
+
+// function addTodo(){
+//   let newElement = document.createElement('li')
+//   newElement.innerText = document.querySelector('input[type="text"]').value
+//   document.querySelector("#tasks").appendChild(newElement);
+// }
+// document.querySelector('#create-task-form').addEventListener("submit",function(e){
+//   e.preventDefault();
+//   addTodo();
+// })
 });
