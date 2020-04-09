9422f5f53c6575f5d82d55cad081c0cf4bdaacfe
Omar Alithawi <oadnan100@gmail.com>
2020-04-09 11:50:29 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  });
 });
