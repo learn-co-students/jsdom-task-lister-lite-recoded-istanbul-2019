daaebe9e0a1fa199116f8651bebff57b1801166f
Refia Candan Ozkalay <refia.candan@gmail.com>
2020-04-10 21:10:55 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector("#create-task-form").addEventListener("click", function(event) {
+    event.preventDefault();
+  }, false);
+  )};
+
