9a860af62684c93a10c301a763f8a92d1097a6f3
basil-kawak <basilalisa0@gmail.com>
2020-04-10 23:05:04 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  //your code here
-  
+function todo(){
+  let creatLi = document.createElement('li');
+  let getValue = document.querySelector('#new-task-description').value;
+  creatLi.innerText = getValue;
+  document.querySelector('#tasks').appendChild(creatLi);
+}
+
+document.querySelector("#create-task-form").addEventListener("submit", function (e){
+e.preventDefault();
+todo();
+})
+});
+
+
