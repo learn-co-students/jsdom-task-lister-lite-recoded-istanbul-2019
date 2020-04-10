43bf30d233eb816541b3b7efba3e0c2966338e88
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-04-11 01:22:14 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
 document.getElementById("submit").addEventListener("click", function(event) {
   event.preventDefault();
 }, false);
+let text = document.getElementById(new-task-description).nodeValue;
+let listItem = text;
+
+function addNewItem() {
+  let listItem = document.createElement("li");
+getElementById(tasks).appendChild(listItem);
+}
