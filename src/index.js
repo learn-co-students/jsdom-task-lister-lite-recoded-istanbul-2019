3ecdebcedf14e8a17f823be6d5d3f5808a9878c2
tugba <ttugbasen@gmail.com>
2020-06-23 03:11:33 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  function newTask() {
+    let task = document.createElement('task')
+    task.innerText = document.getElementById('#new-task-description').value
+    document.getElementById('#tasks').appendChild(task)
+    document.getElementById('#new-task-description').value= ""
+  }
+
+
+  
+document.querySelector("#create-task-form").addEventListener("submit", function (event) {
+         event.preventDefault();
+         newTask()
+})
+
+
+
+})
